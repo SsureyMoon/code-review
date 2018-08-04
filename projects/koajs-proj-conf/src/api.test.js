146e6cbefb8ab68a14cfaaa92f19422ca091f04c
Ssurey Moon <ssureymoon@gmail.com>
2018-08-04 14:16:15 +0900

    [nodejs-review] review-19 fixed. use common setup and teardown in higher describe level

--- projects/koajs-proj-conf/src/api.test.js
+++ projects/koajs-proj-conf/src/api.test.js
@@ -19,7 +19,7 @@ describe('healthcheck test', () => {
     });
 });
 
-describe('books endpoint', () => {
+describe('', () => {
     beforeAll(async () => {
         try {
             await db.collection.create();
@@ -30,71 +30,53 @@ describe('books endpoint', () => {
         }
     });
 
-    test('should return 400 for an invalid request', async () => {
-        const response = await request(app.callback()).post(`/${API_VER}/books`, {});
-        expect(response.status).toEqual(400);
-        expect(response.body).toEqual({
-            name: 'required but not provided',
-            isbn: 'required but not provided',
+    describe('books endpoint', () => {
+        test('should return 400 for an invalid request', async () => {
+            const response = await request(app.callback()).post(`/${API_VER}/books`, {});
+            expect(response.status).toEqual(400);
+            expect(response.body).toEqual({
+                name: 'required but not provided',
+                isbn: 'required but not provided',
+            });
+        });
+
+        test('should save a book in the database and return 201 for a valid request', async () => {
+            const testObject = {
+                name: '리눅스 커널 이야기',
+                isbn: '9788966264049',
+                publishedAt: '2007-08-16',
+            };
+
+            const response = await request(app.callback())
+                .post('/v1/books')
+                .send(testObject)
+                .set('Content-Type', 'application/json');
+            expect(response.status).toEqual(201);
+
+            const docs = await db.collection.all();
+            expect(docs.count).toBe(1);
+
+            const doc = await docs.next();
+            expect(doc).toEqual(expect.objectContaining(testObject));
+            expect(response.status).toEqual(201);
         });
     });
 
-    test('should save a book in the database and return 201 for a valid request', async () => {
-        const testObject = {
-            name: '리눅스 커널 이야기',
-            isbn: '9788966264049',
-            publishedAt: '2007-08-16',
-        };
+    describe('book list endpoint', () => {
+        beforeEach(async () => {
+            await db.collection.import([
+                { name: '리눅스 커널 이야기', isbn: '9788966264049', publishedAt: '2007-08-16' },
+                { name: '전문가를 위한 파이썬', isbn: '9788968484988', publishedAt: '2016-08-12' },
+                { name: 'Python Cookbook', isbn: '9788992649681', publishedAt: '2014-01-17' },
+            ]);
+        });
 
-        const response = await request(app.callback())
-            .post('/v1/books')
-            .send(testObject)
-            .set('Content-Type', 'application/json');
-        expect(response.status).toEqual(201);
-
-        const docs = await db.collection.all();
-        expect(docs.count).toBe(1);
-
-        const doc = await docs.next();
-        expect(doc).toEqual(expect.objectContaining(testObject));
-        expect(response.status).toEqual(201);
-    });
-
-    afterEach(async () => {
-        // delete all docs in the collection after each test
-        await db.collection.truncate();
-    });
-
-    afterAll(async () => {
-        // drop the collection after all tests
-        await db.collection.drop();
-    });
-});
-
-describe('book list endpoint', () => {
-    beforeAll(async () => {
-        try {
-            await db.collection.create();
-        } catch (error) {
-            if (!error.errorNum === 1207) {
-                throw error;
-            }
-        }
-    });
-
-    beforeEach(async () => {
-        await db.collection.import([
-            { name: '리눅스 커널 이야기', isbn: '9788966264049', publishedAt: '2007-08-16' },
-            { name: '전문가를 위한 파이썬', isbn: '9788968484988', publishedAt: '2016-08-12' },
-            { name: 'Python Cookbook', isbn: '9788992649681', publishedAt: '2014-01-17' },
-        ]);
-    });
-
-    test('should return book list', async () => {
-        const response = await request(app.callback())
-            .get(`/${API_VER}/books`);
-        expect(response.status).toEqual(200);
-        expect(response.body).toHaveLength(3);
+        test('should return book list', async () => {
+            const response = await request(app.callback())
+                .get(`/${API_VER}/books`);
+            expect(response.status).toEqual(200);
+            expect(response.body).toHaveLength(3);
+        });
     });
 
     afterEach(async () => {
--- projects/koajs-proj-conf/src/middlewares.test.js
+++ projects/koajs-proj-conf/src/middlewares.test.js
@@ -5,165 +5,142 @@ const {
     handleHealthCheck,
 } = require('./middlewares');
 
-describe('ValidateMiddleware for book item', () => {
-    const next = jest.fn();
-    const middleware = buildValidateMiddleware(validators.bookItemValidators, 'request.body');
+describe('', () => {
     let ctx;
-
-    beforeEach(() => {
-        ctx = {
-            request: {
-                body: {},
-            },
-        };
-    });
-
-    test('should return invalid status when required fields do not exist', async () => {
-        await middleware(ctx, next);
-        expect(ctx.validation.isValid).toBe(false);
-        expect(ctx.validation.messages).toEqual({
-            name: 'required but not provided',
-            isbn: 'required but not provided',
-        });
-        expect(ctx.sanitizedBody).toEqual({});
-    });
-
-    test('should return valid status when required fields are in the right formats', async () => {
-        ctx = {
-            request: {
-                body: {
-                    name: '리눅스 커널 이야기',
-                    isbn: '9788966264049',
-                    publishedAt: '2007-08-16',
-                },
-            },
-        };
-        await middleware(ctx, next);
-        expect(ctx.validation.isValid).toBe(true);
-        expect(ctx.validation.messages).toEqual({});
-        expect(ctx.sanitizedBody).toEqual(ctx.request.body);
-    });
-
-    test('should return invalid status when values are not in the right format', async () => {
-        ctx = {
-            request: {
-                body: {
-                    name: '',
-                    isbn: 'ABC9788966264049XYZ',
-                    publishedAt: '20070816',
-                },
-            },
-        };
-        await middleware(ctx, next);
-        expect(ctx.validation.isValid).toBe(false);
-        expect(ctx.validation.messages).toEqual({
-            name: 'name must not be empty',
-            isbn: 'invalid ISBN',
-            publishedAt: 'invalid time format (YYYY-MM-DD)',
-        });
-        expect(ctx.sanitizedBody).toEqual({});
-    });
-
-    test('should return a body object undefined fields are filtered in', async () => {
-        ctx = {
-            request: {
-                body: {
-                    _key: '9999', // try overriding a key
-                    name: '전문가를 위한 파이썬',
-                    isbn: '9788968484988',
-                    publishedAt: '2016-08-12',
-                },
-            },
-        };
-        await middleware(ctx, next);
-        expect(ctx.validation.isValid).toBe(true);
-        expect(ctx.validation.messages).toEqual({});
-        expect(ctx.sanitizedBody).toEqual({
-            name: '전문가를 위한 파이썬',
-            isbn: '9788968484988',
-            publishedAt: '2016-08-12',
-        });
-    });
-});
-
-describe('Error handling middleware', () => {
     let next;
-    let ctx;
-
-    const middleware = handleExceptions;
 
     beforeEach(() => {
         next = jest.fn();
-
         ctx = {
-            body: '',
             status: '',
-            app: {
-                emit: jest.fn(),
-            },
-        };
-    });
-
-    test('should wrapped the server exception(5xx) message and emit error', async () => {
-        const err = new Error('Error');
-        err.status = 500;
-        err.message = 'stack trace.';
-        next.mockImplementation((() => {
-            throw err;
-        }));
-
-        await middleware(ctx, next);
-        expect(ctx.status).toBe(500);
-        expect(ctx.body).toBe('Internal server error');
-        expect(ctx.app.emit).toHaveBeenCalledWith('error', err, ctx);
-    });
-
-    test('should pass the client exception(4xx)', async () => {
-        const err = new Error('Error');
-        err.status = 400;
-        err.message = 'what the client did wrong';
-        next.mockImplementation((() => {
-            throw err;
-        }));
-
-        await middleware(ctx, next);
-        expect(ctx.status).toBe(400);
-        expect(ctx.body).toBe('what the client did wrong');
-    });
-});
-
-describe('Health checking middleware', () => {
-    let next;
-    let ctx;
-
-    const middleware = handleHealthCheck;
-
-    beforeEach(() => {
-        next = jest.fn();
-
-        ctx = {
+            body: '',
             request: {
                 header: {},
+                body: {},
             },
-            body: '',
-            status: '',
             app: {
                 emit: jest.fn(),
             },
         };
     });
 
-    test('should return 200 when the header user-agent contains elb-healthchecker', async () => {
-        ctx.request.header['user-agent'] = 'ELB-HealthChecker/2.0';
-        await middleware(ctx, next);
-        expect(ctx.status).toBe(200);
-        expect(ctx.body).toEqual({ message: 'ok' });
-        expect(next).not.toHaveBeenCalled();
+    describe('ValidateMiddleware for book item', () => {
+        const middleware = buildValidateMiddleware(validators.bookItemValidators, 'request.body');
+
+        test('should return invalid status when required fields do not exist', async () => {
+            await middleware(ctx, next);
+            expect(ctx.validation.isValid).toBe(false);
+            expect(ctx.validation.messages).toEqual({
+                name: 'required but not provided',
+                isbn: 'required but not provided',
+            });
+            expect(ctx.sanitizedBody).toEqual({});
+        });
+
+        test('should return valid status when required fields are in the right formats', async () => {
+            ctx = {
+                request: {
+                    body: {
+                        name: '리눅스 커널 이야기',
+                        isbn: '9788966264049',
+                        publishedAt: '2007-08-16',
+                    },
+                },
+            };
+            await middleware(ctx, next);
+            expect(ctx.validation.isValid).toBe(true);
+            expect(ctx.validation.messages).toEqual({});
+            expect(ctx.sanitizedBody).toEqual(ctx.request.body);
+        });
+
+        test('should return invalid status when values are not in the right format', async () => {
+            ctx = {
+                request: {
+                    body: {
+                        name: '',
+                        isbn: 'ABC9788966264049XYZ',
+                        publishedAt: '20070816',
+                    },
+                },
+            };
+            await middleware(ctx, next);
+            expect(ctx.validation.isValid).toBe(false);
+            expect(ctx.validation.messages).toEqual({
+                name: 'name must not be empty',
+                isbn: 'invalid ISBN',
+                publishedAt: 'invalid time format (YYYY-MM-DD)',
+            });
+            expect(ctx.sanitizedBody).toEqual({});
+        });
+
+        test('should return a body object undefined fields are filtered in', async () => {
+            ctx = {
+                request: {
+                    body: {
+                        _key: '9999', // try overriding a key
+                        name: '전문가를 위한 파이썬',
+                        isbn: '9788968484988',
+                        publishedAt: '2016-08-12',
+                    },
+                },
+            };
+            await middleware(ctx, next);
+            expect(ctx.validation.isValid).toBe(true);
+            expect(ctx.validation.messages).toEqual({});
+            expect(ctx.sanitizedBody).toEqual({
+                name: '전문가를 위한 파이썬',
+                isbn: '9788968484988',
+                publishedAt: '2016-08-12',
+            });
+        });
     });
 
-    test('should call next when the header user-agent does not contain elb-healthchecker', async () => {
-        ctx.request.header['user-agent'] = 'BlahBlah';
-        await middleware(ctx, next);
-        expect(next).toHaveBeenCalled();
+    describe('Error handling middleware', () => {
+        const middleware = handleExceptions;
+
+        test('should wrapped the server exception(5xx) message and emit error', async () => {
+            const err = new Error('Error');
+            err.status = 500;
+            err.message = 'stack trace.';
+            next.mockImplementation((() => {
+                throw err;
+            }));
+
+            await middleware(ctx, next);
+            expect(ctx.status).toBe(500);
+            expect(ctx.body).toBe('Internal server error');
+            expect(ctx.app.emit).toHaveBeenCalledWith('error', err, ctx);
+        });
+
+        test('should pass the client exception(4xx)', async () => {
+            const err = new Error('Error');
+            err.status = 400;
+            err.message = 'what the client did wrong';
+            next.mockImplementation((() => {
+                throw err;
+            }));
+
+            await middleware(ctx, next);
+            expect(ctx.status).toBe(400);
+            expect(ctx.body).toBe('what the client did wrong');
+        });
+    });
+
+    describe('Health checking middleware', () => {
+        const middleware = handleHealthCheck;
+
+        test('should return 200 when the header user-agent contains elb-healthchecker', async () => {
+            ctx.request.header['user-agent'] = 'ELB-HealthChecker/2.0';
+            await middleware(ctx, next);
+            expect(ctx.status).toBe(200);
+            expect(ctx.body).toEqual({ message: 'ok' });
+            expect(next).not.toHaveBeenCalled();
+        });
+
+        test('should call next when the header user-agent does not contain elb-healthchecker', async () => {
+            ctx.request.header['user-agent'] = 'BlahBlah';
+            await middleware(ctx, next);
+            expect(next).toHaveBeenCalled();
+        });
     });
 });
